cb18e374700127e97ce2304b2822cde50c8e35c0
Rafli andreansyah <47078618+rafliandreansyah@users.noreply.github.com>
2024-12-23 22:55:12 +0700

    setup inertia js and react

---
+++ resources/js/Components/Test.jsx
@@ -0,0 +1,3 @@
+export default function Test() {
+    return <h2>This is test</h2>;
+}
---
+++ resources/js/Pages/Home/Index.jsx
@@ -0,0 +1,12 @@
+import Test from "@/Components/Test";
+
+export default function HomePage() {
+    return (
+        <>
+            <h1 className="text-xl font-bold underline">
+                Home Page From React
+            </h1>
+            <Test />
+        </>
+    );
+}
